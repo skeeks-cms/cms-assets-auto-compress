9a882fdb8814f3a0611fd8db05a7374224c18682
Semenov <semenov@skeeks.com>
2015-08-06 00:59:13 +0300

    start dev

---
+++ AssetsAutoCompressComponent.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (бъшъб)
+ * @date 06.08.2015
+ */
+namespace skeeks\yii2\assetsAuto;
+
+class AssetsAutoCompressComponent extends \skeeks\yii2\assetsAuto\AssetsAutoCompressComponent
+{
+
+}
---
+++ config/main.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (ÑêèêÑ)
+ * @date 15.06.2015
+ */
+return [
+    'bootstrap'    => ['assetsAutoCompress'],
+
+    'components' =>
+    [
+        'assetsAutoCompress' =>
+        [
+            'class'         => '\skeeks\cms\assetsAuto\AssetsAutoCompressComponent',
+        ],
+    ],
+];
