4b336b446b61964804bd7359d4a6a92969b6cbfe
Semenov <semenov@skeeks.com>
2015-08-06 01:05:12 +0300

    start dev

--- AssetsAutoCompressComponent.php
+++ AssetsAutoCompressComponent.php
@@ -5,9 +5,32 @@
  * @copyright 2010 SkeekS (ÑêèêÑ)
  * @date 06.08.2015
  */
-namespace skeeks\yii2\assetsAuto;
+namespace skeeks\cms\assetsAuto;
 
+/**
+ * Class AssetsAutoCompressComponent
+ * @package skeeks\cms\assetsAuto
+ */
 class AssetsAutoCompressComponent extends \skeeks\yii2\assetsAuto\AssetsAutoCompressComponent
 {
+    public function bootstrap($app)
+    {
+        if ($app instanceof Application)
+        {
+            $app->view->on(View::EVENT_END_PAGE, function(Event $e)
+            {
+                /**
+                 * @var $view View
+                 */
+                $view = $e->sender;
 
+                if ($this->enabled && $view instanceof View && \Yii::$app->response->format == Response::FORMAT_HTML && !\Yii::$app->request->isAjax && !\Yii::$app->request->isPjax)
+                {
+                    \Yii::beginProfile('Compress assets');
+                    $this->_processing($view);
+                    \Yii::endProfile('Compress assets');
+                }
+            });
+        }
+    }
 }
